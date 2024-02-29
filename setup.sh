d881e2d2c0cadc12da932a5a0af9fc2712efe114
avouacr <avouacr@pm.me>
2024-02-29 17:03:41 +0000

    lessgo

--- setup.sh
+++ setup.sh
@@ -1,6 +1,12 @@
-rm -rf ~/.TinyTeX
-sudo apt update
-sudo apt-get install -y texlive 
-sudo apt-get install -y texlive-fonts-extra 
+#!/bin/bash
+set -e
+
+if ! command -v pdflatex; then
+    rm -rf ~/.TinyTeX
+    sudo apt update
+    sudo apt-get install -y texlive 
+    sudo apt-get install -y texlive-fonts-extra 
+fi
+
 pdflatex authorsample.tex        
 pdflatex authorsample.tex
