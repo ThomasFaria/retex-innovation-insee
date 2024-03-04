80841d2e60a64bf6fd2ec90fb2f3a1bd5c9efb58
ThomasFaria <thomas.faria@ensae.fr>
2024-03-04 15:18:59 +0000

    correct bugs

--- setup.sh
+++ setup.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 set -e
 
-if ! command -v pdflatex; then
+DIR="~/.TinyTeX"
+if [ -d "$DIR" ]; then
+  # Take action if $DIR exists.
     rm -rf ~/.TinyTeX
     sudo apt update
     sudo apt-get install -y texlive 
     sudo apt-get install -y texlive-fonts-extra 
 fi
 
+pdflatex authorsample.tex        
+bibtex authorsample.aux
 pdflatex authorsample.tex        
 pdflatex authorsample.tex
